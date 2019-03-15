9023d6dd95a0a62ce5451ae8bc2ec6b2abe7c734
rimas <rmocius@gmail.com>
2019-03-15 16:57:32 -0700

    .

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -5,6 +5,7 @@ if [ -n "${HELM_LOGIN_PLUGIN_NO_INSTALL_HOOK}" ]; then
     exit 0
 fi
 
+# shellcheck disable=SC2002
 version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
 echo "Downloading and installing helm-linter v${version} ..."
 
@@ -17,12 +18,13 @@ else
     url="https://github.com/rimusz/helm-linter/releases/download/v${version}/helm-linter_${version}_windows_amd64.tar.gz"
 fi
 
-echo $url
+echo "$url"
 
 mkdir -p "bin"
 mkdir -p "releases/v${version}"
 
 # Download with curl if possible.
+# shellcheck disable=SC2230
 if [ -x "$(which curl 2>/dev/null)" ]; then
     curl -sSL "${url}" -o "releases/v${version}.tar.gz"
 else
@@ -31,4 +33,3 @@ fi
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/bin/helmlinter" "bin/helmlinter" || \
     mv "releases/v${version}/bin/helmlinter.exe" "bin/helmlinter"
-
